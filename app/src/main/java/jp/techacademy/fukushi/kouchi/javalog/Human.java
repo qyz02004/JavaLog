3eae836aae30f23a91d4afb0f76ba37e679f1125
kouchi.fukushi <qyz02004@gmail.com>
2018-01-15 21:01:49 +0900

    think目セッドを持つThinkableインターフェースを作成。 Humanクラスに実装。 Humanクラスのコンストラクタを更新し、第3引数にhobbyを追加。

--- app/src/main/java/jp/techacademy/fukushi/kouchi/javalog/Human.java
+++ app/src/main/java/jp/techacademy/fukushi/kouchi/javalog/Human.java
@@ -2,18 +2,26 @@ package jp.techacademy.fukushi.kouchi.javalog;
 
 import android.util.Log;
 
-class Human extends Animal {
+class Human extends Animal implements Thinkable {
     // クラス変数
     static String to_jp = "人間";
+    String hobby;
 
     // コンストラクタ
-    public Human(String name, int age) {
+    public Human(String name, int age, String hobby) {
         this.name = name;
         this.age = age;
+        this.hobby = hobby;
     }
+
     @Override
     public void say() {
             Log.d("javatest", "「私の名前は" + this.name + "です。年は" + this.age + "歳です。」");
+    }
+
+    @Override
+    public void think() {
+        Log.d("javatest", "「私は" + this.hobby + "について考える」");
     }
 }
 
--- app/src/main/java/jp/techacademy/fukushi/kouchi/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/fukushi/kouchi/javalog/MainActivity.java
@@ -2,7 +2,6 @@ package jp.techacademy.fukushi.kouchi.javalog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;    // ここを追加
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +10,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Human human = new Human("山田太郎", 33);     // 名前を山田太郎、年齢33歳で、Humanのインスタンスを作る
+        Human yamada = new Human("山田太郎", 33,"読書");     // 名前を山田太郎、年齢33歳、趣味は読書で、Humanのインスタンスを作る
+        Human suzuki = new Human("鈴木次郎", 43,"スポーツ");     // 名前を鈴木次郎、年齢43歳、趣味はスポーツで、Humanのインスタンスを作る
 
-        human.say();    // 話す
+        yamada.say();    // 話す
+        yamada.think();    // 考える
+
+        suzuki.say();    // 話す
+        suzuki.think();    // 考える
      }
 }
